2d3d2a5c0ad35d58ec972c56d40337f7c6a5f03b
cirochang <cirochang@live.com>
2015-10-21 19:48:17 -0200

    refactore code.

--- caminhante_bebado.py
+++ caminhante_bebado.py
@@ -2,12 +2,12 @@ import random
 import matplotlib.pyplot as plt
 
 position = []
-media = []
-position.append(0)
-for i in range(1000):
+position.append(0)	#position start from 0
+for i in range(1000):	#numbers of interations
 	x = random.randint(0,1)
 	if x==0:
 		x=-1
-	position.append(position[-1] + x)
-plt.plot(position)
-plt.show()
+	position.append(position[-1] + x)	#increment position
+
+plt.plot(position)	#prepare plot
+plt.show()	#show plot
