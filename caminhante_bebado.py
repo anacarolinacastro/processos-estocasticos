8bf9dbe04ec0fe2a6b071984a9325fb2fb84dc0c
Ciro Chang <cirochang@live.com>
2015-10-30 21:39:24 -0200

    caminhante bebado 1d finish

--- caminhante_bebado.py
+++ caminhante_bebado.py
@@ -1,13 +1,48 @@
 import random
+import math
 import matplotlib.pyplot as plt
 
-position = []
-position.append(0)	#position start from 0
-for i in range(1000):	#numbers of interations
-	x = random.randint(0,1)
-	if x==0:
-		x=-1
-	position.append(position[-1] + x)	#increment position
+def media(x):
+	return sum(x)/len(x)
 
-plt.plot(position)	#prepare plot
-plt.show()	#show plot
+def desvio_padrao(x):
+	soma = 0
+	media_x = media(x)
+	for i in range(len(x)):
+		soma += (x[i] - media_x)**2
+	return math.sqrt(soma/(len(x)))
+
+def main():
+	caminhante = []
+	desvio_do_caminhante =[]
+	t = []
+
+	for j in range(1000):
+		t.append(math.sqrt(j))
+
+	for j in range(200):
+		position = []
+		position.append(0)	#position start from 0
+
+		for i in range(1000):	#numbers of interations
+			x = random.randint(0,1)
+			if x==0:
+				x=-1.0
+			else:
+				x=1.0
+			position.append(position[-1] + x)	#increment position
+
+		caminhante.append(position)
+
+	for j in range(len(caminhante[0])):
+		amostra = []
+		for z in range(len(caminhante)):
+			amostra.append(caminhante[z][j])	
+		desvio_do_caminhante.append(desvio_padrao(amostra))	
+
+	plt.plot(desvio_do_caminhante)	#prepare plot
+	plt.plot(t)
+	plt.show()	#show plot
+	return 0
+
+main()
