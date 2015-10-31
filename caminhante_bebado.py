8fdf6720f4a648e9b115925ae604ba1ad3e814cf
Ana Carolina Castro <anacarolina@Anas-MacBook-Air.local>
2015-10-30 22:49:38 -0200

    hehe

--- caminhante_bebado.py
+++ caminhante_bebado.py
@@ -4,21 +4,47 @@ import matplotlib.pyplot as plt
 from pylab import cm, imshow, colorbar, show
 
 def umaDimensao():
-	position = []
-	position.append(0)	#position start from 0
-	for i in range(1000):	#numbers of interations
-		x = random.randint(0,1)
-		if x==0:
-			x=-1
-		position.append(position[-1] + x)	#increment position
+	caminhante = []
+	desvio_do_caminhante =[]
+	t = []
 
-	plt.plot(position)	#prepare x plot
+	for j in range(1000):
+		t.append(math.sqrt(j))
+
+	for j in range(200):
+		position = []
+		position.append(0)	#position start from 0
+
+		for i in range(1000):	#numbers of interations
+			x = random.randint(0,1)
+			if x==0:
+				x=-1.0
+			else:
+				x=1.0
+			position.append(position[-1] + x)	#increment position
+
+		caminhante.append(position)
+
+	for j in range(len(caminhante[0])):
+		amostra = []
+		for z in range(len(caminhante)):
+			amostra.append(caminhante[z][j])	
+		desvio_do_caminhante.append(desvio_padrao(amostra))	
+
+	plt.plot(desvio_do_caminhante)	#prepare plot
+	plt.plot(t)
 	plt.show()	#show plot
 
 
 def duasDimensoes():
-	amostras = 100
+	amostras = 1000
 	tempo = 100
+
+	desvio_do_caminhante =[]
+	tp = []
+
+	for j in range(tempo):
+		tp.append(math.sqrt(j))
 
 	matrixX = []
 	matrixY = []
@@ -68,8 +94,6 @@ def duasDimensoes():
 				else:
 					y = 1.0
 				positionY.append(positionY[-1] + y)	#increment position
-
-
 		matrixX.append(positionX)
 		matrixY.append(positionY)
 
@@ -92,13 +116,22 @@ def duasDimensoes():
 		for t in range(tempo):
 			matrix[int(int(abs(minY))+matrixY[i][t])][int(int(abs(minX))+matrixX[i][t])] += 1
 
-	# colormap
-	cmap = cm.jet
-	# set NaN values as white
-	cmap.set_bad('w')
-	im = imshow(matrix, cmap=cmap, interpolation='nearest')
-	colorbar()
-	show()
+	for j in range(len(matrix[0])):
+		amostra = []
+		for z in range(len(matrixX)):
+			amostra.append(matrixX[z][j])	
+		desvio_do_caminhante.append(desvio_padrao(amostra))	
+
+	plt.plot(desvio_do_caminhante)	#prepare plot
+	plt.plot(tp)
+	plt.show()	#show plot
+
+	# # colormap
+	# cmap = cm.jet
+	# cmap.set_bad('w')
+	# im = imshow(matrix, cmap=cmap, interpolation='nearest')
+	# colorbar()
+	# show()
 
 def media(x):
 	return sum(x)/len(x)
@@ -143,7 +176,6 @@ def caminhate1D_ciro():
 	plt.show()	#show plot
 
 def main():
-	# caminhate1D_ciro()
 	# umaDimensao()
 	duasDimensoes()
 	
